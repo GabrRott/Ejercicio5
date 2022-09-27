ee60d68434875dc749aaf842f286f0ee537f02ff
GabrRott <gabrielroatta@gmail.com>
2022-09-27 09:33:06 -0300

    Number variables

---
+++ Ficha-personal.js
@@ -0,0 +1,9 @@
+const hight_cm = 192;
+const hight_m = 1.92;
+let weight_k = 81.2;
+let hight_round = hight_m.toFixed(0);
+let weight_round = parseInt(weight_k);
+let max_valor =  Number.MAX_VALUE+1;
+
+
+console.log(max_valor == Number.MAX_VALUE);
